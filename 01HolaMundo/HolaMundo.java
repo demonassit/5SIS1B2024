eda1517d1671c3173e910197ebf18a4d1e197723
demonassit <jminorg@ipn.mx>
2024-05-20 10:25:51 -0600

    mi primer programa

---
+++ 01HolaMundo/HolaMundo.java
@@ -0,0 +1,17 @@
+public class HolaMundo{
+ /*
+ Los metodos son comportmaientos que va a tener una clase u objeto
+
+ existen metodos principales main
+ metodos getter and setter que sirven para el intercambio de datos a partir del encapsulamiento
+ metodos constructores que sirven para la estructura de la clase, inicializar las variables de la clase asi como las instancias de la misma
+ metodos por tipo de datos, cuando nos referimos a los tipo int, float, double, String, Boolean
+
+ los objetos son instancias tambien conocidas como invocaciones lo que significa que son la construccion abstracta de las cosas
+
+ los atributos de una clase son las caracteristicas de la clase u objeto
+ */   
+    public static void main(String[] args){
+        System.out.println("Hola mundo mundial");
+    }
+}
